70718ec84fe269183c8dcda115b359e20d9ee978
timurmataev <mataevtimur68@gmail.com>
2024-09-03 10:24:04 +0600

    Addition company model

---
+++ sed/Models/Company.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace sed.Models;
+
+public class Company
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Название не указано")]
+    [StringLength(70, MinimumLength = 1, ErrorMessage = "Диапазон от 1 до 70")]
+    public string? Name { get; set; }
+
+    public List<User> Users { get; set; } = new List<User>();
+}
